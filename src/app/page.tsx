c6604d104d3d8d836c5184fa56735d3460fd1b4c
Vitaliy <vitas.mishenko@gmail.com>
2024-02-20 12:29:18 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { useState } from "react";
 
 export default function Home() {
+  const theme = useState('default');
   return (
-    <main>
+    <main className={`${styles.main} ${theme}`}>
       <button className={styles.button}>Test</button>
     </main>
   );
