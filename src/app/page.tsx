3c04417cd492041db13258132d75f2e5809c6fea
Vitaliy <vitas.mishenko@gmail.com>
2024-02-21 14:56:20 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,15 @@ import { useState } from "react";
 export default function Home() {
   const theme = useState('default');
   return (
-    <main className={`${styles.main} ${theme}`}>
-      <button className={styles.button}>Test</button>
+    <main className={styles.main}>
+      {/* <button className={styles.button}>Test</button> */}
+      <div className={styles.card}>
+        <div className={styles.image}>
+          <Image src={'/IMG.png'} layout="fill" alt="zavod"/>
+        </div>
+        <span>Трубопрокатный завод</span>
+        <button>Подробнее</button>
+      </div>
     </main>
   );
 }
